75a9c09b0103f3365ea16ff39253284f877ec0e8
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-04-07 16:51:33 -0300

    Subindo exercicio 10

--- ex10.js
+++ ex10.js
@@ -1,4 +1,4 @@
-const readline = requie ("readline-sync")
+const readline = require ("readline-sync")
 
 
 let livro = 300 
@@ -7,8 +7,7 @@ let leitor = readline.questionInt ("quantas páginas ja foram lidas? ")
 
 let diferenca = livro - leitor
 
-if (leitor >= 300) {
-    console.log ("voce terminou o livro, parabens!")
-} else {
+
+console.log ("--------LEITURA--------")
+
     console.log (`voce ja leu ${Math.round((leitor/livro) * 100)}% do livro, faltam ${diferenca} páginas para terminar!`)
-}
