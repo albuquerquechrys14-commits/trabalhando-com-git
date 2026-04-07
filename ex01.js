b9d0713150aca3f1272e89606967e83682498bae
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-04-07 15:09:32 -0300

    feat: adicionei exercicio 1

---
+++ ex01.js
@@ -0,0 +1,14 @@
+const readline = require('readline-sync')
+
+// let nome = readline.question("Digite seu Nome: ")
+
+// console.log(`Seu nome é: ${nome}`)
+
+let nome = readline.question("Digite o nome do Aluno: ")
+console.log (`O nome do Aluno é: ${nome}`)
+let curso = readline.question("Qual o curso que o Aluno está fazendo?: ")
+console.log(`O curso de ${nome} é: ${curso}`)
+let idade = readline.question("Digite a idade do aluno: ")
+console.log(`A idade de ${nome} é: ${idade}`)
+
+
