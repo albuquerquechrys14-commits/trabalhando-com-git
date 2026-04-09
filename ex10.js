f7e52f7b5b7f5a522adcb74dea719e1c494a20d7
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-04-09 08:28:41 -0300

    feat: Subindo exercicio 10

--- ex10.js
+++ ex10.js
@@ -5,13 +5,15 @@ let livro = 300
 console.log("-------------------------------")
 console.log("|------------BIBLIOTECA---------|")
 console.log("==------------------------------")
-let cliente = readline.question ("qual o livro que alugou?")
-
+let cliente = readline.question ("qual o livro que alugou? ")
+console.log ("                   ")
 console.log("|------------LIVRO---------|")
 
 let leitor = readline.questionInt ("quantas páginas ja foram lidas? ")
 
 let diferenca = livro - leitor
+
+console.log("                            ")
 
 console.log ("--------LEITURA--------")
 
