26f3d25d4405be178559db10f8b484b0214d51f4
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-04-14 13:23:41 -0300

    feat: Subindo exercicio 3

--- ex03.js
+++ ex03.js
@@ -3,13 +3,13 @@ const readline = require ('readline-sync')
 
 let nickname = readline.question ("Informe o Seu Nome de Jogador: ")
 let jogoFavorito = readline.question ("Jogo favorito do usuario: ")
-let Pontuacao = readline.questionInt ("Pontuacao Atual do jogador: ")
+let pontuacao = readline.questionInt ("Pontuacao Atual do jogador: ")
 
 
 console.log ("===========PERFIL DE JOGADOR=========")
 
 console.log (`O nickname do Jogador é: ${nickname}`)
 console.log (`O jogo Favorito do Jogador é: ${jogoFavorito}`)
-console.log (`Pontuacao Atual do Jogador: ${Pontuacao}`)
+console.log (`Pontuacao Atual do Jogador: ${pontuacao}`)
 
 
