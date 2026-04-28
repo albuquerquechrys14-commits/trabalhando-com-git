4faa8d81836812d4e4b0b701a50ec99c655b1d8d
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-04-28 15:12:09 -0300

    feat: Subindo exercicio 15

---
+++ ex15.js
@@ -0,0 +1,16 @@
+const readline = require ('readline-sync')
+
+console.log ("===========CONSUMO DE CALORIAS=========")
+
+let limiteDiario = 2000
+
+let calorias = readline.questionInt ("Quantas calorias foram consumidas no dia?: ")
+let percentual = (calorias / limiteDiario) * 100
+console.log ("")
+console.log ("===========RESULTADO DO DIA===========")
+
+if (calorias == limiteDiario) {
+    console.log (`Você está dentro do limite.`)
+} else {
+    console.log (`Você ultrapassou ${percentual.toFixed(2)}% do limite.`)
+}
