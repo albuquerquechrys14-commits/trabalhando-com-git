98e157d3fb512bd2e79a09984f58f6b06c33e48d
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-04-28 14:45:17 -0300

    feat: Subindo exercicio 14

---
+++ ex14.js
@@ -0,0 +1,14 @@
+const readline = require ('readline-sync')
+
+console.log ("===========META DE VENDAS=========")
+
+let metaDiaria = 5000
+let vendaDiaria = readline.questionFloat ("valor total faturado no dia (R$): ")
+let percentual = (vendaDiaria / metaDiaria) * 100
+let diferenca = metaDiaria - vendaDiaria
+
+if (vendaDiaria <= metaDiaria) {
+    console.log (`O mercado não atingiu a meta diária. Faltou ${((diferenca / metaDiaria) * 100).toFixed(2)}%.`)
+}else {
+    console.log (`Parabéns! O mercado atingiu a meta diária. Você bateu (ou excedeu) a meta em ${((vendaDiaria - metaDiaria) / metaDiaria * 100).toFixed(2)}%.`)
+}
